184de48de8928058437a58b846c1d78a5c25e5dd
igrimzyi <1isaiahofficial1@gmail.com>
2023-01-11 17:39:29 -0800

    migrate to webpack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
+import { BrowserRouter, Router } from "react-router-dom";
+import './index.css'
 
-import './index.css';
 
+ReactDOM.render(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+ 
+, document.querySelector('#root'));
